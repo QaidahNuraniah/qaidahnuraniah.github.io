df0d43c8849a17505e30ba68f6876ec4a9c3bb20
syifarahmat.id <syifarahmat.id@gmail.com>
2026-08-14 15:45:53 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# qaidahnuraniah.github.io
---
+++ bash.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Buat folder 'resourceFilePath' jika belum ada
+mkdir -p resourceFilePath
+cd resourceFilePath
+
+# Download semua file ke dalam folder tersebut
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1646555800306.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1646556089017.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1646639566219.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1646736423052.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1646736755726.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1646737650370.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1646738832703.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1647372925232.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1647427696866.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1647670390947.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1649064524329.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1649113002404.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1660071397001.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1661615802056.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1661694208538.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1661694883329.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1661695419750.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1661799161913.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1686827568887.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1739801597726.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1739801718100.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1739801853451.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1739802153749.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1739802187775.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1739802225190.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1740402857880.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/page-1740404725892.zip
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1646555803285.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1646556092099.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1646639569334.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1646736426551.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1646736758327.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1646737653275.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1646738835123.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1647372928074.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1647427698420.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1647670393790.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1649064527561.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1649113005163.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1660071400586.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1661615838433.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1661694307045.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1661695140760.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1661695793600.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1661799162607.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1686827569436.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1739801598388.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1739801718739.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1739801854657.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1739802154922.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1739802188852.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1739802226589.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1740402858354.html
+curl -O https://api.qaidahnuraniah.com/page/resource/resource-1740404727122.html
